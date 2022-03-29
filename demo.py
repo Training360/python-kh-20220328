88a8ee5e2a778c6fa5e2c44aa9d9c03c494d585d
Istvan Viczian <viczian.istvan@gmail.com>
2022-03-29 16:39:33 +0200

    Mod

--- demo.py
+++ demo.py
@@ -3,3 +3,5 @@ name = ""
 # Rövidzár
 starts = len(name) > 1 and name[0].lower() == "j"
 print(starts)
+
+# mod2
--- hello.py
+++ hello.py
@@ -4,3 +4,5 @@ print("Hello Docker")
 
 response = requests.get("https://api.github.com/users/vicziani")
 print(response.json())
+
+# mod1
